6ab5ee32e77840035ae2277655095bbcae551a64
yyhchen <yuehuasolo@outlook.com>
2024-09-05 14:52:32 +0800

    Accelerate + DeepSpeed Training

---
+++ distributed_train/DeepSpeed/ddp_accelerate_advanced.py
@@ -0,0 +1,159 @@
+import time
+import math
+import torch
+import pandas as pd
+
+from torch.optim import Adam
+from accelerate import Accelerator
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from peft import LoraConfig, get_peft_model
+from transformers import BertTokenizer, BertForSequenceClassification
+
+
+model_path = "/root/private_data/models/hfl/chinese-macbert-large"
+
+class MyDataset(Dataset):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = pd.read_csv("./ChnSentiCorp_htl_all.csv")
+        self.data = self.data.dropna()
+
+    def __getitem__(self, index):
+        return self.data.iloc[index]["review"], self.data.iloc[index]["label"]
+    
+    def __len__(self):
+        return len(self.data)
+
+
+def prepare_dataloader():
+
+    dataset = MyDataset()
+
+    trainset, validset = random_split(dataset, lengths=[0.9, 0.1], generator=torch.Generator().manual_seed(42))
+
+    tokenizer = BertTokenizer.from_pretrained(model_path)
+
+    def collate_func(batch):
+        texts, labels = [], []
+        for item in batch:
+            texts.append(item[0])
+            labels.append(item[1])
+        inputs = tokenizer(texts, max_length=128, padding="max_length", truncation=True, return_tensors="pt")
+        inputs["labels"] = torch.tensor(labels)
+        return inputs
+
+    trainloader = DataLoader(trainset, batch_size=32, collate_fn=collate_func, shuffle=True)
+    validloader = DataLoader(validset, batch_size=64, collate_fn=collate_func, shuffle=False)
+
+    return trainloader, validloader
+
+
+def prepare_model_and_optimizer():
+
+    model = BertForSequenceClassification.from_pretrained(model_path)
+
+    lora_config = LoraConfig(target_modules=["query", "key", "value"])
+
+    model = get_peft_model(model, lora_config)
+
+    model.print_trainable_parameters()
+
+    optimizer = Adam(model.parameters(), lr=2e-5)
+
+    return model, optimizer
+
+
+def evaluate(model, validloader, accelerator: Accelerator):
+    model.eval()
+    acc_num = 0
+    # with torch.inference_mode():
+    with torch.no_grad():
+        for batch in validloader:
+            output = model(**batch)
+            pred = torch.argmax(output.logits, dim=-1)
+            pred, refs = accelerator.gather_for_metrics((pred, batch["labels"]))
+            acc_num += (pred.long() == refs.long()).float().sum()
+    return acc_num / len(validloader.dataset)
+
+
+def train(model, optimizer, trainloader, validloader, accelerator: Accelerator, resume, epoch=3, log_step=10):
+    global_step = 0
+    start_time = time.time()
+
+    resume_step = 0
+    resume_epoch = 0
+
+    # 断点保存， 加载检查点，计算跳过的轮数和步数，数据集跳过对应步数
+    if resume is not None:
+        accelerator.load_state(resume)
+        steps_per_epoch = math.ceil(len(trainloader) / accelerator.gradient_accumulation_steps)
+        resume_step = global_step = int(resume.split("step_")[-1])
+        resume_epoch = resume_step // steps_per_epoch
+        resume_step -= resume_epoch * steps_per_epoch
+        accelerator.print(f"resume from checkpoint -> {resume}")
+
+    for ep in range(resume_epoch, epoch):
+        model.train()
+
+        # 判断是否断点保存
+        if resume and ep == resume_epoch and resume_step != 0:
+            active_dataloader = accelerator.skip_first_batches(trainloader, resume_step * accelerator.gradient_accumulation_steps)
+        else:
+            active_dataloader = trainloader
+        
+        for batch in active_dataloader:
+            with accelerator.accumulate(model):
+                optimizer.zero_grad()
+                output = model(**batch)
+                loss = output.loss
+                accelerator.backward(loss)
+                optimizer.step()
+
+                # 梯度累加，模型保存
+                if accelerator.sync_gradients:
+                    global_step += 1
+
+                    if global_step % log_step == 0:
+                        loss = accelerator.reduce(loss, "mean")
+                        accelerator.print(f"ep: {ep}, global_step: {global_step}, loss: {loss.item()}")
+                        accelerator.log({"loss": loss.item()}, global_step)
+
+                    # （分布式）模型保存
+                    if global_step % 50 == 0 and global_step != 0:
+                        accelerator.print(f"save checkpoint -> step_{global_step}")
+                        accelerator.save_state(accelerator.project_dir + f"/step_{global_step}")
+                        accelerator.unwrap_model(model).save_pretrained(
+                            save_directory=accelerator.project_dir + f"/step_{global_step}/model",
+                            is_main_process=accelerator.is_main_process,
+                            state_dict=accelerator.get_state_dict(model),
+                            save_func=accelerator.save
+                        )
+        acc = evaluate(model, validloader, accelerator)
+        accelerator.print(f"ep: {ep}, acc: {acc}, time: {time.time() - start_time}")
+        accelerator.log({"acc": acc}, global_step)
+
+    accelerator.end_training()
+
+
+def main():
+
+    # 设置梯度累加
+    accelerator = Accelerator(gradient_accumulation_steps=2, log_with="tensorboard", project_dir="ckpts")
+
+    accelerator.init_trackers("runs")
+
+    trainloader, validloader = prepare_dataloader()
+
+    model, optimizer = prepare_model_and_optimizer()
+
+    model, optimizer, trainloader, validloader = accelerator.prepare(model, optimizer, trainloader, validloader)
+
+    # resume 放想要断点开始训练的路径，默认是 None，也就是从头训练
+    train(model, optimizer, trainloader, validloader, accelerator, resume=None)
+
+
+if __name__ == "__main__":
+    main()
--- distributed_train/README.md
+++ distributed_train/README.md
@@ -95,3 +95,168 @@ accelerate launch --mixed_precision bf16 ddp_accelerate_advanced.py
 ![accelerate ddp_accelerate](/assets/ddp_accelerate_advanced_result.png)
 
 > tensorboard 功能可以通过 `command + shift + p` 输入 `tensorboard` 打开
+
+
+
+<br>
+<br>
+
+
+
+### DeepSpeed + accelerate 
+
+在 `accelerate` 中使用 `DeepSpeed` 集成，详情可参考[官方文档](https://huggingface.co/docs/accelerate/usage_guides/deepspeed)
+
+训练代码用的仍然是 [ddp_accelerate_advanced.py](/distributed_train/DeepSpeed/ddp_accelerate_advanced.py)
+
+
+**执行方法:**
+
+先配置 `accelerate config`:
+![accelerate config by use deepspeed](/assets/accelerate_config_deepspeed.png)
+
+> 在上图中，将 **DeepSpeed's ZeRO optimization stage** 选择为 2, 是因为2比3的速度更快, 前提是一张卡能放入整个模型的情况下，选择 3 会拆分模型，增加通信时间延长模型训练时间.
+
+将配置生成的 [`default_config.yaml`](/distributed_train/DeepSpeed/default_config.yaml) 文件复制到当前目录下, 然后执行:
+
+```bash
+accelerate launch --config_file default_config.yaml ddp_accelerate_advanced.py
+```
+
+**效果图如下:**
+![deepspeed result](/assets/ddp_deepspeed_result.png)
+
+
+<br>
+<br>
+
+
+### 使用 DeepSpeed与 没有使用 DeepSpeed 的 accelerate 对比
+
+没有使用 `DeepSpeed`:
+
+![no deepspeed](/assets/no_deepspeed_result.png)
+
+
+
+使用 `DeepSpeed`:
+![use deepspeed](/assets/deepspeed_result.png)
+
+
+可以看到时间还增加了几秒，可能是多了通信时间吧，在 `epoch` 大的时候才能拉开差距
+
+
+<br>
+<br>
+
+
+### DeepSpeed 与 Accelerate 使用的一些细节
+
+> **Tips:**
+> `.yaml` 是 `accelerate` 使用的配置文件， `.json` 才是 `DeepSpeed` 使用的配置文件
+
+<br>
+<br>
+
+**使用 stage2：**
+
+之前使用 `accelerate config` 进行配置的时候，并没有指定 `DeepSpeed` 的 `config` 文件，我们可以从 `accelerate` [官方文档](https://huggingface.co/docs/accelerate/usage_guides/deepspeed) 中找到如何配置
+
+
+在 [`default_config.yaml`](/distributed_train/DeepSpeed/default_config.yaml) 基础上，修改为 [`default_config_modify_zero2.yaml`](/distributed_train/DeepSpeed/default_config_modify_zero2.yaml), 并根据官方案例 编写 [`zero_stage2_config.json`](/distributed_train/DeepSpeed/zero_stage2_config.json), 删去了 `optimizer` 和 `scheduler` 部分, 并且将 `fp16` 修改为 `bf16`, 删除其余参数，只保留 `"enabled": true`
+
+
+> 官网的 `config` 文件案例有点问题，指定了 `deepspeed_config_file` 需要注释掉 `mixed_precision: bf16`，否则会报错
+
+
+<br>
+<br>
+
+**使用 stage3：**
+
+- 方式1:
+
+**不使用 `deepspeed_config_file`, 直接修改 `zero_stage: 3`.**
+
+如果按照之前的配置执行代码会报梯度相关的错误，原因是 `stage3` 是将模型拆分进行训练的，需要在 `deepspeed_config:` 中增加 `zero3_save_16bit_model: true`, 详细请看 [`default_config_zero3.yaml`](/distributed_train/DeepSpeed/default_config_zero3.yaml)
+
+
+还有一个问题就是在 执行代码 `ddp_acclerate_advanced.py` 结束一个 `epoch` 后报错，原因是在 `ddp_acclerate_advanced.py` 中使用了 `torch.inference_mode` 完全去掉了梯度，只要改为 `torch.no_grad` 即可.
+
+错误如下所示：
+![no grad error](/assets/no_grad_error.png)
+
+> 这个错误引出了 `torch.inference_mode` 与 `torch.no_grad` 的区别
+
+
+<br>
+
+- 方式2:
+
+**用 `deepspeed_config_file` 方式 启动 `stage3`.**
+
+
+修改 `deepspeed_config_file` 为 `zero_stage3_config.json`, 在 `zero_optimization`部分 修改参数为 `"stage": 3`, 并且增加参数 `"stage3_gather_16bit_weights_on_mode_save": true`
+
+
+<br>
+<br>
+
+
+**更细粒度的使用方式：**
+
+`zero stage2`, 执行 :
+```bash
+accelerate launch --config_file default_config.yaml ddp_accelerate_advanced.py
+```
+
+`zero stage2` 指定 deepspeed config file 形式, 执行 :
+```bash
+accelerate launch --config_file default_config_modify_zero2.yaml ddp_accelerate_advanced.py
+```
+
+
+`zero stage3` 以此类推.
+
+> **Tips:** `zero2` 我命名为 `default_config.yaml`, `zero3` 命名为 `default_config_zero3.yaml`
+
+
+<br>
+<br>
+
+
+**额外地:**
+
+如果直接使用 `tranformers` 封装好的代码 [`ddp_transformers.py`](/distributed_train/distributed_data_parallel/ddp_transformers.py), 同样的直接执行:
+
+```bash
+accelerate launch --config_file default_config.yaml ddp_transformers.py
+```
+
+训练过程如下图所示:
+
+![trainer in deepspeed](/assets/deepspeed_trainer_result.png)
+
+感觉还是 `transformers` 一套集成起来的好看～
+
+
+config文件根据自己的需要进行修改即可, 需要注意的是 `ddp_transformers` 中的 `TrainingArguments` 参数里面配置的值必须与 `default_config.yaml` 文件的信息一直，否则会报错～
+
+
+
+<br>
+<br>
+
+### Accelerate && DeepSpeed 多机多卡使用
+
+两种情况:
+
+- 情景1:
+
+直连情况下，指定 `deepspeed_hostfile`, `num_machines`, `num_process`, `main_process_port`, 正常通过 `Accelerate` 启动
+
+<br>
+
+- 情景2:
+
+slurm 管理情况下, 配置启动器为 `torchrun` 标准的启动器, 指定 `num_machines`, `machine_rank`, `num_process`, `main_process_port`, `main_process_ip`, 还是以 `Accelerate` 启动, 模式与 `torchrun` 启动类似
